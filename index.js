c59177cc995a6f988d51667adb1ff3bf97a55ef4
Zafkiel0 <zafkiel468@gmail.com>
2022-11-06 21:21:47 -0600

    v-if

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = new Vue ({
     el:'#app',
     data:{
-        titulo:'Frutas',frutas:['Uva','Manzana','Pera']
+        titulo:'Frutas',frutas:[{nombre:'Uva',cantidad:20},
+        {nombre:'Manzana',cantidad:20},
+        {nombre:'Fresa',cantidad:20},
+        {nombre:'Naranja',cantidad:0}]
     }
 })
