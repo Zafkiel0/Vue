56d8bbcabf26b087158d4959959e3b6f8782b1ab
Zafkiel0 <zafkiel468@gmail.com>
2022-11-06 21:34:54 -0600

    metodos

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = new Vue ({
         titulo:'Frutas',frutas:[{nombre:'Uva',cantidad:20},
         {nombre:'Manzana',cantidad:20},
         {nombre:'Fresa',cantidad:20},
-        {nombre:'Naranja',cantidad:0}]
+        {nombre:'Naranja',cantidad:0}],nuevaFruta:''
+    },methods:{
+        agregarFruta(){
+            this.frutas.push({
+                nombre:this.nuevaFruta,cantidad:0
+            })
+        }
+
     }
 })
