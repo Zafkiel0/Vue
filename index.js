312c03df9d4b5d5d87542b9b7715ed25b861ee3b
Zafkiel0 <zafkiel468@gmail.com>
2022-11-06 22:40:49 -0600

    cmputed,me falta arregale lo de sumar

--- index.js
+++ index.js
@@ -4,14 +4,24 @@ const app = new Vue ({
         titulo:'Frutas',frutas:[{nombre:'Uva',cantidad:20},
         {nombre:'Manzana',cantidad:20},
         {nombre:'Fresa',cantidad:20},
-        {nombre:'Naranja',cantidad:0}],nuevaFruta:''
+        {nombre:'Naranja',cantidad:0}],
+        nuevaFruta:'',total: 0
     },methods:{
         agregarFruta(){
             this.frutas.push({
                 nombre:this.nuevaFruta,cantidad:0
             });
-            this.nuevaFruta=''
+            this.nuevaFruta='';
         }
 
+    },
+    computed:{
+            sumarFrutas() { 
+                this.total=0;
+                for(frut of this.frutas){
+                    this.total+= fruta.cantidad;
+                }
+              return console.log("hola");        
+                }
     }
 })
